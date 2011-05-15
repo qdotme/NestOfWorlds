a0ad712a5cdc98c34d40a606d7fc9117b9ef2755
Marcin Kaszynski <marcin@setjam.com>
2011-05-15 17:52:29 +0200

    Fixed restart.sh.

--- nest/restart.sh
+++ nest/restart.sh
@@ -1,2 +1,2 @@
-ps eax| grep manage.py | grep port=5999|cut -d' ' -f1| xargs kill
+ps eax| grep manage.py | grep port=5999|cut -d' ' -f1| sed -e 's/^ *//' | xargs kill
 ./manage.py runfcgi maxchildren=1 host=127.0.0.1 port=5999
